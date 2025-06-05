2bc8e5afdf60e2159102ec51b421bdf3b185926e
Devlrxxh <seabearvalorant@gmail.com>
2025-06-05 16:34:14 +0200

    Made the hearts overlap like vanilla

--- src/main/java/dev/lrxh/mcuiexample/HealthUIComponent.java
+++ src/main/java/dev/lrxh/mcuiexample/HealthUIComponent.java
@@ -6,9 +6,6 @@ import dev.lrxh.mcui.elements.Element;
 import dev.lrxh.mcui.elements.ElementSpace;
 import org.bukkit.entity.Player;
 
-import java.util.Arrays;
-import java.util.stream.Stream;
-
 public class HealthUIComponent extends UIComponent {
 
     private final Element full;
@@ -30,22 +27,20 @@ public class HealthUIComponent extends UIComponent {
     @Override
     public void tick(Player player) {
         double hearts = player.getHealth() / 2;
-        Element[] bar = new Element[10];
+        Element[] bar = new Element[11];
+        bar[0] = ElementSpace.BACKSPACE_48.createElement();
 
-        for (int i = 0; i < 10; i++) {
-            if (hearts >= i + 1) {
+        for (int i = 1; i <= 10; i++) {
+            if (hearts >= i) {
                 bar[i] = full;
-            } else if (hearts >= i + 0.5) {
+            } else if (hearts >= i - 0.5) {
                 bar[i] = half;
             } else {
                 bar[i] = empty;
             }
         }
 
-        DefaultUIRender.ACTION_BAR.render(player, Stream.concat(
-                Stream.of(ElementSpace.BACKSPACE_48.createElement()),
-                Arrays.stream(bar)
-        ).toArray(Element[]::new));
+        DefaultUIRender.ACTION_BAR.render(player, bar);
 
     }
 }
