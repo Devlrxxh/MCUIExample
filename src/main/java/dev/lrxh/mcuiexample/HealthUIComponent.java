0ff4c1ff10057bafa0fc7961cb1daafd47d471e7
Devlrxxh <seabearvalorant@gmail.com>
2025-06-05 17:34:29 +0200

    Put the heath ui on above hunger

--- src/main/java/dev/lrxh/mcuiexample/HealthUIComponent.java
+++ src/main/java/dev/lrxh/mcuiexample/HealthUIComponent.java
@@ -13,13 +13,13 @@ public class HealthUIComponent extends UIComponent {
     private final Element empty;
 
     public HealthUIComponent() {
-        full = register("https://i.imgur.com/tfqo2Pt.png", 27, 2);
-        half = register("https://i.imgur.com/7UwHZoK.png", 27, 2);
-        empty = register("https://i.imgur.com/fw0RByQ.png", 27, 2);
+        full = register("https://i.imgur.com/tfqo2Pt.png", 27, 3);
+        half = register("https://i.imgur.com/7UwHZoK.png", 27, 3);
+        empty = register("https://i.imgur.com/fw0RByQ.png", 27, 3);
 
-        full.setElementSpace(ElementSpace.BACKSPACE_2);
-        half.setElementSpace(ElementSpace.BACKSPACE_2);
-        empty.setElementSpace(ElementSpace.BACKSPACE_2);
+        full.setElementSpace(ElementSpace.BACKSPACE_1);
+        half.setElementSpace(ElementSpace.BACKSPACE_1);
+        empty.setElementSpace(ElementSpace.BACKSPACE_1);
 
         load();
     }
@@ -27,20 +27,24 @@ public class HealthUIComponent extends UIComponent {
     @Override
     public void tick(Player player) {
         double hearts = player.getHealth() / 2;
-        Element[] bar = new Element[11];
-        bar[0] = ElementSpace.BACKSPACE_48.createElement();
+        Element[] bar = new Element[12];
+        bar[0] = ElementSpace.FORWARDSPACE_48.createElement();
+        bar[1] = ElementSpace.FORWARDSPACE_1.createElement();
 
-        for (int i = 1; i <= 10; i++) {
-            if (hearts >= i) {
-                bar[i] = full;
-            } else if (hearts >= i - 0.5) {
-                bar[i] = half;
+        for (int i = 0; i < 9; i++) {
+            int barIndex = 10 - i;
+            double heartIndex = i + 1;
+
+            if (hearts >= heartIndex) {
+                bar[barIndex] = full;
+            } else if (hearts >= heartIndex - 0.5) {
+                bar[barIndex] = half;
             } else {
-                bar[i] = empty;
+                bar[barIndex] = empty;
             }
         }
 
         DefaultUIRender.ACTION_BAR.render(player, bar);
-
     }
+
 }
